3cdbff1b531f5534bf42ff31c15b8818ccb9b6bf
laiharry <lai.harry@gmail.com>
2019-08-06 11:22:12 +0800

    Add files via upload

--- js/wcConnect.js
+++ js/wcConnect.js
@@ -3,8 +3,8 @@ class WCConnect {
 	constructor() {
         var socket = null;
 //        this.PORT =  8000;
-//        this.ip ='localhost';
-        this.IP ='rpsserver-rps.apps.us-east-2.online-starter.openshift.com';
+        this.ip ='localhost';
+//        this.IP ='rpsserver-rps.apps.us-east-2.online-starter.openshift.com';
 //        this.IP ='apps.us-east-2.online-starter.openshift.com';
 //    this.PORT = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8000;
     this.PORT = 8000;
